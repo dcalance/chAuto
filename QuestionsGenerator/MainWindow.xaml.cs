0da70ce020ab0db48ae11d03731e8c2adaa4e8b5
dcalance <daniel.calancea@faf.utm.md>
2017-09-01 20:52:10 +0300

    Added binary serializer tool for generating questions.

---
+++ QuestionsGenerator/MainWindow.xaml.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace QuestionsGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int currentQuestionBoxes = 1;
+        List<List<Question>> chapters = new List<List<Question>>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Init();
+        }
+        private void newChapterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            chapters.Add(new List<Question>() { new Question() });
+            updateChapterList();
+        }
+        private void updateChapterList()
+        {
+            while (chaptersList.Items.Count > 0)
+            {
+                chaptersList.Items.RemoveAt(0);
+            }
+            for (int i = 0; i < chapters.Count; i++)
+            {
+                chaptersList.Items.Add("Chapter" + (i + 1));
+            }
+            chaptersList.SelectedIndex = 0;
+        }
+        private void updateQuestionList()
+        {
+            while (questionsList.Items.Count > 0)
+            {
+                questionsList.Items.RemoveAt(0);
+            }
+
+            if (chaptersList.SelectedIndex == -1) //kastily
+            {
+                chaptersList.SelectedIndex = 0;
+            }
+
+            for (int i = 0; i < chapters[chaptersList.SelectedIndex].Count; i++)
+            {
+                questionsList.Items.Add("Question" + (i + 1));
+            }
+            questionsList.SelectedIndex = 0;
+        }
+        private void updateFields()
+        {
+            questionBox.Text = chapters[chaptersList.SelectedIndex][questionsList.SelectedIndex].questionText;
+            imageBox.Text = chapters[chaptersList.SelectedIndex][questionsList.SelectedIndex].imageName;
+            correctAnswerBox.Text = chapters[chaptersList.SelectedIndex][questionsList.SelectedIndex].correctAnswer.ToString();
+            if (chapters[chaptersList.SelectedIndex][questionsList.SelectedIndex].answers != null)
+            {
+                while (chapters[chaptersList.SelectedIndex][questionsList.SelectedIndex].answers.Length > currentQuestionBoxes)
+                {
+                    addAnswerBtn.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+                }
+                while (chapters[chaptersList.SelectedIndex][questionsList.SelectedIndex].answers.Length < currentQuestionBoxes)
+                {
+                    removeAnswerBtn.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+                }
+                for (int i = 0; i < chapters[chaptersList.SelectedIndex][questionsList.SelectedIndex].answers.Length; i++)
+                {
+                    TextBox element = FindChild<TextBox>(Application.Current.MainWindow, "answerBox" + (i + 1));
+                    element.Text = chapters[chaptersList.SelectedIndex][questionsList.SelectedIndex].answers[i];
+                }
+            }
+            else
+            {
+                answerBox1.Text = "";
+                while (currentQuestionBoxes > 1)
+                {
+                    removeAnswerBtn.RaiseEvent(new RoutedEventArgs(Button.ClickEvent));
+                }
+            } 
+        }
+        private void saveChanges(int selectedChapter, int selectedQuestion)
+        {
+            List<string> answers = new List<string>();
+            chapters[selectedChapter][selectedQuestion].questionText = questionBox.Text;
+            chapters[selectedChapter][selectedQuestion].imageName = imageBox.Text;
+            int result;
+            int.TryParse(correctAnswerBox.Text, out result);
+            chapters[selectedChapter][selectedQuestion].correctAnswer = result;
+            int fieldNr = 1;
+            TextBox element = FindChild<TextBox>(Application.Current.MainWindow, "answerBox" + fieldNr);
+            while (element != null)
+            {
+                fieldNr += 1;
+                answers.Add(element.Text);
+                element = FindChild<TextBox>(Application.Current.MainWindow, "answerBox" + fieldNr);
+            }
+            chapters[selectedChapter][selectedQuestion].answers = answers.ToArray();
+        }
+
+        private void chaptersList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (chaptersList.IsKeyboardFocusWithin)
+            {
+                ListBox prev = (ListBox)sender;
+                if (chaptersList.Items.Count > 0)
+                {
+                    updateQuestionList();
+                    updateFields();
+                    //saveChanges(prev.SelectedIndex, questionsList.SelectedIndex);
+                }
+            }
+        }
+        private void newQuestionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            chapters[chaptersList.SelectedIndex].Add(new Question());
+            updateQuestionList();
+        }
+        private void deleteChapterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (chapters.Count > 1)
+            {
+                chapters.RemoveAt(chaptersList.SelectedIndex);
+                chaptersList.SelectedIndex = 0;
+                updateChapterList();
+                updateQuestionList();
+                updateFields();
+            }
+        }
+
+        private void addAnswerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            currentQuestionBoxes += 1;
+            stackPanel.Children.Add(new Label() {Content = "Answer " + currentQuestionBoxes });
+            stackPanel.Children.Add(new TextBox() { Name = "answerBox" + currentQuestionBoxes });
+        }
+
+        private void removeAnswerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentQuestionBoxes > 1)
+            {
+                currentQuestionBoxes -= 1;
+                for (int i = 0; i < 2; i++)
+                {
+                    stackPanel.Children.RemoveAt(stackPanel.Children.Count - 1);
+                }
+            }
+        }
+
+        private void questionsList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (questionsList.IsKeyboardFocusWithin)
+            {
+                ListBox prev = (ListBox)sender;
+                updateFields();
+                //saveChanges(chaptersList.SelectedIndex, prev.SelectedIndex);
+            }
+        }
+
+        private void saveChangesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            saveChanges(chaptersList.SelectedIndex, questionsList.SelectedIndex);
+        }
+
+        private void deleteQuestionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (chapters[chaptersList.SelectedIndex].Count > 1)
+            {
+                chapters[chaptersList.SelectedIndex].RemoveAt(questionsList.SelectedIndex);
+                questionsList.SelectedIndex = 0;
+                updateQuestionList();
+                updateFields();
+            }
+        }
+        private void Init()
+        {
+            chapters.Add(new List<Question>());
+            updateChapterList();
+
+            chapters[0].Add(new Question());
+            updateQuestionList();
+        }
+
+        private void clearAllBtn_Click(object sender, RoutedEventArgs e)
+        {
+            chapters = new List<List<Question>>();
+            Init();
+            updateFields(); 
+        }
+
+        private void upChapterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (chaptersList.SelectedIndex > 0)
+            {
+                SwapChapters(chaptersList.SelectedIndex, chaptersList.SelectedIndex - 1);
+                chaptersList.SelectedIndex -= 1;
+                updateQuestionList();
+            }
+        }
+        private void downChapterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (chaptersList.SelectedIndex < chaptersList.Items.Count - 1 && chaptersList.SelectedIndex >= 0)
+            {
+                SwapChapters(chaptersList.SelectedIndex, chaptersList.SelectedIndex + 1);
+                chaptersList.SelectedIndex += 1;
+                updateQuestionList();
+            }
+        }
+        private void SwapChapters(int index1, int index2)
+        {
+            List<Question> temp = chapters[index1];
+            chapters[index1] = chapters[index2];
+            chapters[index2] = temp;
+        }
+        private void SwapQuestions(int index1, int index2)
+        {
+            Question temp = chapters[chaptersList.SelectedIndex][index1];
+            chapters[chaptersList.SelectedIndex][index1] = chapters[chaptersList.SelectedIndex][index2];
+            chapters[chaptersList.SelectedIndex][index2] = temp;
+        }
+
+        private void upQuestionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (questionsList.SelectedIndex > 0)
+            {
+                SwapQuestions(questionsList.SelectedIndex, questionsList.SelectedIndex - 1);
+                questionsList.SelectedIndex -= 1;
+                updateFields();
+            }
+        }
+
+        private void downQuestionBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (questionsList.SelectedIndex < questionsList.Items.Count - 1 && questionsList.SelectedIndex >= 0)
+            {
+                SwapQuestions(questionsList.SelectedIndex, questionsList.SelectedIndex + 1);
+                questionsList.SelectedIndex += 1;
+                updateFields();
+            }
+        }
+        public static T FindChild<T>(DependencyObject parent, string childName)
+        where T : DependencyObject
+        {
+            // Confirm parent and childName are valid. 
+            if (parent == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child. 
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    // If the child's name is set for search
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        // if the child's name is of the request name
+                        foundChild = (T)child;
+                        break;
+                    }
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
+
+        private void saveAsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".text"; // Default file extension
+            dlg.Filter = "Binary File (.bin)|*.bin"; // Filter files by extension
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+                List<Question[]> a = new List<Question[]>();
+                int maxElements = 0;
+                foreach (var item in chapters)
+                {
+                    a.Add(item.ToArray());
+                    if (item.Count > maxElements)
+                    {
+                        maxElements = item.Count;
+                    }
+                }
+                Question[][] final = new Question[chapters.Count][];
+                for (int i = 0; i < a.Count; i++)
+                {
+                    final[i] = a[i];
+                }
+
+                IFormatter formatter = new BinaryFormatter();
+                using (Stream stream = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    formatter.Serialize(stream, final);
+                }
+            }
+        }
+    }
+}
---
+++ QuestionsGenerator/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestionsGenerator
+{
+    [Serializable]
+    public class Question
+    {
+        public string questionText { get; set; }
+        public string imageName { get; set; }
+        public int correctAnswer { get; set; }
+        public string[] answers { get; set; }
+    }
+}
